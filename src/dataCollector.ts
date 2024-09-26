18269e8f22f56dfa6a14313eb52ae2cfa509b8d1
Andrew Morris <voltrevo@gmail.com>
2024-09-26 17:06:40 +1000

    crude way to delete games

--- src/dataCollector.ts
+++ src/dataCollector.ts
@@ -8,6 +8,12 @@ export type DataItem = {
   to: BoardJson,
 };
 
+type Game = {
+  start: number,
+  end: number,
+  score: number,
+};
+
 class DataCollector {
   size = new LocalStorageUnit('dataCollector-size', 0);
 
@@ -32,18 +38,74 @@ class DataCollector {
 
   all() {
     const res: DataItem[] = [];
+    let skips = 0;
 
     for (let i = 0; i < this.size.get(); i++) {
+      const item = this.at(i).get();
+
+      if (item === undefined) {
+        skips++;
+        continue;
+      }
+
+      res.push(item);
+    }
+
+    if (skips > 0) {
+      console.log(`Skipped ${skips} items`);
+    }
+
+    return res;
+  }
+
+  findGames() {
+    let gameStart = 0;
+    let games = [];
+
+    const len = this.size.get();
+    let prevItem: DataItem | undefined = undefined;
+    
+    for (let i = gameStart; i < len; i++) {
       const item = this.at(i).get();
 
       if (item === undefined) {
         throw new Error('missing item');
       }
 
-      res.push(item);
+      if (
+        (prevItem !== undefined && prevItem.from.score > 0) &&
+        item.from.score === 0
+      ) {
+        games.push({
+          start: gameStart,
+          end: i,
+          score: prevItem.to.score,
+        });
+
+        gameStart = i;
+      }
+
+      prevItem = item;
     }
 
-    return res;
+    games.sort((a, b) => a.score - b.score);
+
+    return games;
+  }
+
+  deleteGame(game: Game) {
+    const gameLen = game.end - game.start;
+    const len = this.size.get();
+
+    for (let i = game.end; i < len; i++) {
+      this.at(i - gameLen).set(this.at(i).get());
+    }
+
+    this.size.set(len - gameLen);
+
+    for (let i = 0; i < gameLen; i++) {
+      this.at(len - i - 1).set(undefined);
+    }
   }
 }
 
